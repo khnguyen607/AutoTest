a257e615139a24a908cfdad35e2574ff9f36bee4
khnguyen607 <khnguyen.job@gmail.com>
2024-07-08 14:11:59 +0700

    done..

--- cypress/tests/product.spec.js
+++ cypress/tests/product.spec.js
@@ -4,111 +4,41 @@ describe('Products', () => {
     });
 
     // Test case: Truy cập vào trang sản phẩm sau khi tìm kiếm sản phẩm
-    // it('Truy cập vào trang sản phẩm sau khi tìm kiếm sản phẩm', () => {
-    //     // Truy cập vào trang tìm kiếm
-    //     cy.get('#twotabsearchtextbox').then(() => {
-    //         // Danh sách các sản phẩm cần tìm kiếm
-    //         const products = ['laptop', 'smartphone', 'TV']
-
-    //         // Lặp qua mỗi sản phẩm trong danh sách
-    //         products.forEach(product => {
-    //             // Nhập sản phẩm vào trường tìm kiếm
-    //             cy.get('#twotabsearchtextbox').type(product);
-    //             // Nhấn nút tìm kiếm
-    //             cy.get('#nav-search-submit-button').click();
-    //             // Kiểm tra kết quả tìm kiếm có chứa sản phẩm được tìm
-    //             cy.get('[data-component-type="s-result-info-bar"]').should('contain', product);
-    //             // Lấy thông tin về sản phẩm đầu tiên trong kết quả tìm kiếm
-    //             cy.get("div[data-index='3']").find('h2').as('productTitle');
-    //             // Lấy giá của sản phẩm đầu tiên trong kết quả tìm kiếm
-    //             cy.get("div[data-index='3'] .a-price .a-price-symbol").siblings().invoke('text').then((price) => {
-    //                 // Lấy tiêu đề của sản phẩm đầu tiên trong kết quả tìm kiếm
-    //                 cy.get('@productTitle').invoke('text').then((productTitleContent) => {
-    //                     // Click vào sản phẩm đầu tiên để truy cập vào trang chi tiết sản phẩm
-    //                     cy.get('@productTitle').find('a').click();
-    //                     // Kiểm tra tiêu đề trang chi tiết sản phẩm có chứa tiêu đề của sản phẩm
-    //                     cy.get('#productTitle').invoke('text').should('contain', productTitleContent);
-    //                     // Kiểm tra giá trên trang chi tiết sản phẩm có khớp với giá trong kết quả tìm kiếm
-    //                     cy.get("#corePrice_feature_div .a-price .a-price-symbol").siblings().invoke('text').should('contain', price);
-    //                 })
-    //             })
-    //             // Quay lại trang tìm kiếm
-    //             cy.go('back');
-    //             cy.go('back');
-    //         })
-    //     })
-    // });
-
-    // Truy cập vào trang sản phẩm sau khi lọc theo một danh mục
-    it('Truy cập vào trang sản phẩm sau khi lọc theo một danh mục', () => {
+    it('Truy cập vào trang sản phẩm sau khi tìm kiếm sản phẩm', () => {
         // Truy cập vào trang tìm kiếm
-        cy.get('#searchDropdownBox').then(() => {
-            // Lấy danh sách danh mục sản phẩm
-            const category = ['Computers', 'Amazon Resale'];
+        cy.get('#twotabsearchtextbox').then(() => {
+            // Danh sách các sản phẩm cần tìm kiếm
+            const products = ['laptop', 'smartphone']
 
-            // Lặp qua mỗi danh mục
-            for (var i = 0; i < category.length; i++) {
-                // Chọn danh mục sản phẩm
-                cy.get('#searchDropdownBox').select(category[i], { force: true });
+            // Lặp qua mỗi sản phẩm trong danh sách
+            products.forEach(product => {
+                // Nhập sản phẩm vào trường tìm kiếm
+                cy.get('#twotabsearchtextbox').type(product);
+                // Nhấn nút tìm kiếm
                 cy.get('#nav-search-submit-button').click();
-                cy.get('#nav-search-label-id').should('contain', category[i]);
-
-                // Lấy thông tin về sản phẩm đầu tiên trên trang
-                cy.get("ol[class='a-carousel'] > li").first().as('productTitle');
-                cy.get("ol[class='a-carousel'] > li .a-price .a-price-symbol").first().siblings().invoke('text').then((price) => {
-                    cy.log(price);
-                    cy.get('@productTitle').find('.a-truncate-cut').invoke('text').then((productTitleContent) => {
-                        const splitIndex = productTitleContent.indexOf('.');
-                        const trimmedTitle = productTitleContent.substring(20, splitIndex).trim();
-                        // Click vào sản phẩm đầu tiên
+                // Mô phỏng thao tác vuốt lên và xuống để kiểm tra có lăn chuột được hay không
+                cy.get('div[data-index="3"]').first().trigger('mouseover').trigger('mousedown').trigger('mousemove', { clientY: 100 }).trigger('mousemove', { clientY: -100 }).trigger('mouseup');
+                // Kiểm tra kết quả tìm kiếm có chứa sản phẩm được tìm
+                cy.get('[data-component-type="s-result-info-bar"]').should('contain', product);
+                // Lấy thông tin về sản phẩm đầu tiên trong kết quả tìm kiếm
+                cy.get("div[data-index='3']").find('h2').as('productTitle');
+                // Lấy giá của sản phẩm đầu tiên trong kết quả tìm kiếm
+                cy.get("div[data-index='3'] .a-price .a-price-symbol").siblings().invoke('text').then((price) => {
+                    // Lấy tiêu đề của sản phẩm đầu tiên trong kết quả tìm kiếm
+                    cy.get('@productTitle').invoke('text').then((productTitleContent) => {
+                        // Click vào sản phẩm đầu tiên để truy cập vào trang chi tiết sản phẩm
                         cy.get('@productTitle').find('a').click();
-                        // Kiểm tra tiêu đề sản phẩm
-                        cy.get('#productTitle').invoke('text').should('include', trimmedTitle);
-                        // Kiểm tra giá sản phẩm
-                        cy.log(cy.get("#corePrice_feature_div .a-price .a-offscreen").invoke('text'))
-                        cy.get("#corePrice_feature_div .a-price .a-offscreen").invoke('text').should('contain', price);
-                        // cy.get('.a-box-inner').as('priceBox').then(() => {
-                        //     // Kiểm tra giá sản phẩm
-                        //     cy.get('@priceBox').find("#corePrice_feature_div .a-price .a-price-symbol").then(() => {
-                        //         cy.get("#corePrice_feature_div .a-price .a-price-symbol").siblings().invoke('text').should('contain', price);
-                        //     })
-                        // });
+                        // Kiểm tra tiêu đề trang chi tiết sản phẩm có chứa tiêu đề của sản phẩm
+                        cy.get('#productTitle').invoke('text').should('contain', productTitleContent);
+                        // Kiểm tra giá trên trang chi tiết sản phẩm có khớp với giá trong kết quả tìm kiếm
+                        cy.get("#corePrice_feature_div .a-price .a-price-symbol").siblings().invoke('text').should('contain', price);
                     })
-                });
-                // Quay lại trang trước
+                })
+                // Quay lại trang tìm kiếm
                 cy.go('back');
                 cy.go('back');
-            }
-        });
+            })
+        })
     });
 
-    // // Cần tạo một trường hợp khác để tìm kiếm sách vì giá trên trang sản phẩm được hiển thị khác với các sản phẩm khác
-    // it('Truy cập đến sách', () => {
-    //     cy.get('#searchDropdownBox').then(() => {
-    //         const category = ['Books'];
-
-    //         for (var i = 0; i < category.length; i++) {
-    //             cy.get('#searchDropdownBox').select(category[i], { force: true });
-    //             cy.get('#nav-search-submit-button').click();
-    //             cy.get('#s-refinements').should('contain', category[i]);
-    //             cy.get("ol[class='a-carousel'] > li").first().as('productTitle');
-    //             cy.get("ol[class='a-carousel'] > li .a-price .a-price-symbol").first().siblings().invoke('text').then((price) => {
-    //                 cy.get('@productTitle').find('.a-truncate-cut').invoke('text').then((productTitleContent) => {
-    //                     const splitIndex = productTitleContent.indexOf('.');
-    //                     const trimmedTitle = productTitleContent.substring(20, splitIndex).trim();
-    //                     cy.get('@productTitle').find('a').click();
-    //                     cy.get('#productTitle').invoke('text').should('include', trimmedTitle);
-    //                     cy.get('.a-box-inner').as('priceBox').then(() => {
-    //                         cy.get('@priceBox').find("#price").then(() => {
-    //                             cy.get("#price").invoke('text').should('contain', price);
-    //                         })
-
-    //                     });
-    //                 })
-    //             });
-    //             cy.go('back');
-    //             cy.go('back');
-    //         }
-    //     });
-    // })
 });
--- cypress/tests/searchProducts.spec.js
+++ cypress/tests/searchProducts.spec.js
@@ -8,7 +8,7 @@ describe('Xác thực chức năng tìm kiếm sản phẩm trên website Amazon
     // Tìm kiếm sản phẩm trên website
     it('Tìm kiếm sản phẩm trên website', () => {
         cy.get('#twotabsearchtextbox').then(() => {
-            const product = ['fone de ouvido', 'Mochila', 'Notebook'];
+            const product = ['Laptop', 'SmartPhone', 'Notebook'];
 
             for (var i = 0; i < product.length; i++) {
                 cy.get('#twotabsearchtextbox').type(product[i]); // Nhập từ khóa tìm kiếm
@@ -22,12 +22,11 @@ describe('Xác thực chức năng tìm kiếm sản phẩm trên website Amazon
     // Tìm kiếm theo danh mục sản phẩm
     it('Tìm kiếm theo danh mục sản phẩm', () => {
         cy.get('#searchDropdownBox').then(() => {
-            const category = ['Alexa Skills', 'Beleza', 'Casa'];
+            const category = ['Computers', 'Amazon Resale'];
 
             for (var i = 0; i < category.length; i++) {
                 cy.get('#searchDropdownBox').select(category[i], { force: true }); // Chọn danh mục sản phẩm
                 cy.get('#nav-search-submit-button').click(); // Nhấn nút tìm kiếm
-                cy.get('#s-refinements').should('contain', category[i]); // Kiểm tra trong thanh thông báo tìm kiếm có danh mục hay không
                 cy.go('back'); // Quay lại trang trước
             }
         });
@@ -36,14 +35,14 @@ describe('Xác thực chức năng tìm kiếm sản phẩm trên website Amazon
     // Lọc sản phẩm theo giá: Thấp đến cao
     it('Lọc sản phẩm theo giá: Thấp đến cao', () => {
         cy.get('#twotabsearchtextbox').then(() => {
-            const product = ['Caderno', 'Smartphone'];
+            const product = ['Laptop', 'Smartphone'];
             for (var i = 0; i < product.length; i++) {
                 cy.get('#twotabsearchtextbox').type(product[i]); // Nhập từ khóa tìm kiếm
                 cy.get('#nav-search-submit-button').click(); // Nhấn nút tìm kiếm
                 cy.get('[data-component-type="s-result-info-bar"]').should('contain', product[i]); // Kiểm tra trong thanh thông báo tìm kiếm có sản phẩm hay không
 
-                cy.get('#s-result-sort-select').select('Preço: Do menor para o maior', { force: true }); // Chọn lọc giá từ thấp đến cao
-                cy.get('.a-dropdown-prompt').should('contain', 'Preço: Do menor para o maior'); // Kiểm tra xem đã chọn đúng lọc giá hay chưa
+                cy.get('#s-result-sort-select').select('Price: Low to High', { force: true }); // Chọn lọc giá từ thấp đến cao
+                cy.get('.a-dropdown-prompt').should('contain', 'Price: Low to High'); // Kiểm tra xem đã chọn đúng lọc giá hay chưa
                 cy.go('back'); // Quay lại trang trước
                 cy.go('back'); // Quay lại trang trước
             }
@@ -59,8 +58,8 @@ describe('Xác thực chức năng tìm kiếm sản phẩm trên website Amazon
                 cy.get('#nav-search-submit-button').click(); // Nhấn nút tìm kiếm
                 cy.get('[data-component-type="s-result-info-bar"]').should('contain', product[i]); // Kiểm tra trong thanh thông báo tìm kiếm có sản phẩm hay không
 
-                cy.get('#s-result-sort-select').select('Preço: Do maior para o menor', { force: true }); // Chọn lọc giá từ cao đến thấp
-                cy.get('.a-dropdown-prompt').should('contain', 'Preço: Do maior para o menor'); // Kiểm tra xem đã chọn đúng lọc giá hay chưa
+                cy.get('#s-result-sort-select').select('Price: High to Low', { force: true }); // Chọn lọc giá từ cao đến thấp
+                cy.get('.a-dropdown-prompt').should('contain', 'Price: High to Low'); // Kiểm tra xem đã chọn đúng lọc giá hay chưa
                 cy.go('back'); // Quay lại trang trước
                 cy.go('back'); // Quay lại trang trước
             }
@@ -76,8 +75,8 @@ describe('Xác thực chức năng tìm kiếm sản phẩm trên website Amazon
                 cy.get('#nav-search-submit-button').click(); // Nhấn nút tìm kiếm
                 cy.get('[data-component-type="s-result-info-bar"]').should('contain', product[i]); // Kiểm tra trong thanh thông báo tìm kiếm có sản phẩm hay không
 
-                cy.get('#s-result-sort-select').select('Méd. Avaliações de clientes', { force: true }); // Chọn lọc đánh giá trung bình
-                cy.get('.a-dropdown-prompt').should('contain', 'Méd. Avaliações de clientes'); // Kiểm tra xem đã chọn đúng lọc đánh giá hay chưa
+                cy.get('#s-result-sort-select').select('Avg. Customer Review', { force: true }); // Chọn lọc đánh giá trung bình
+                cy.get('.a-dropdown-prompt').should('contain', 'Avg. Customer Review'); // Kiểm tra xem đã chọn đúng lọc đánh giá hay chưa
                 cy.go('back'); // Quay lại trang trước
                 cy.go('back'); // Quay lại trang trước
             }
